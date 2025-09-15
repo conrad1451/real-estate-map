1b7b65f6de70ccb52b823e8f54012df03ac66e1c
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-09-15 10:03:36 -0400

    feat: new button

--- app/page.tsx
+++ app/page.tsx
@@ -49,6 +49,21 @@ export default function Home() {
           >
             Read our docs
           </a>
+          <a
+            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
+            href="https://conradhqportfolio.vercel.app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <Image
+              className="dark:invert"
+              src="/vercel.svg"
+              alt="Vercel logomark"
+              width={20}
+              height={20}
+            />
+            Check out my 3D portfolio!
+          </a>
         </div>
       </main>
       <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
