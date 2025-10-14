0a6d2a544cb0bec82b8cddb50fda04340068bfc3
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-10-14 15:11:59 -0400

    feat: new areas of page
    feat: sidebar and content and header and footer areas

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,40 @@
+import "./pagestyle.css";
+
+const MyApp = function () {
+  // return <div className="header"> </div>;
+
+  let myCondition = true;
+
+  return (
+    <>
+      <div className="App">
+        <div className="header">
+          <h1>Hello, Next.js!</h1>
+        </div>
+        <div className="container">
+          <div className="sidebar">
+            <>
+              <h2>Sidebar content</h2>
+            </>
+          </div>
+
+          <div className="content">
+            <h2>Main Content</h2>
+          </div>
+        </div>
+        <div className="footer">
+          <h3>The footer is here!</h3>
+        </div>
+      </div>
+    </>
+  );
+};
+
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return (
+    <>
+      {/* <h1>Hello, Next.js!</h1> */}
+      <MyApp />
+    </>
+  );
 }
