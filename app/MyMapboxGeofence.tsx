d2355630d357c3a7dcc8d4e32475bcec5109bd67
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-10-28 14:31:57 -0400

    chore(cleanup): Remove failed attempt MapMoveEvent
    chore(cleanup): Remove failed attempt for MapMoveEvent

--- app/MyMapboxGeofence.tsx
+++ app/MyMapboxGeofence.tsx
@@ -1,7 +1,15 @@
 "use client";
 import * as React from "react";
 // import Map from "react-map-gl/maplibre";
-import Map from "react-map-gl/mapbox";
+
+// CHQ: Gemini AI imported MapEvent for types
+import Map, { MapEvent } from "react-map-gl/mapbox";
+
+// CHQ: Gemini AI corrected itself by importing MapMoveEvent for types
+// import Map, { MapMoveEvent } from "react-map-gl/mapbox";
+// error: '"react-map-gl/mapbox"' has no exported member named 'MapMoveEvent'. Did you mean 'MapMouseEvent'?ts(2724)
+
+// import Map, { MapMouseEvent } from "react-map-gl/mapbox";
 
 // npm install @turf/turf
 import * as turf from "@turf/turf";
@@ -24,14 +32,24 @@ export function MyMapboxGeofence() {
   const MAPBOX_TOKEN =
     process.env.NEXT_PUBLIC_MAPBOX_KEY || "YOUR_MAPBOX_PUBLIC_TOKEN_HERE";
 
-  const onMove = React.useCallback(({ viewState }) => {
-    const newCenter = [viewState.longitude, viewState.latitude];
+  // Use the correct MapEvent type from react-map-gl
+
+  //   MapMouseEvent
+  //   const onMove = React.useCallback((event: MapMouseEvent) => {
+  //   const onMove = React.useCallback((event: MapMoveEvent) => {
+  const onMove = React.useCallback((event: MapEvent) => {
+    // Destructure the new viewState from the event object
+    const newViewState: AView = event.viewState;
+    // Property 'viewState' does not exist on type 'MapMouseEvent'.ts(2339)
+
+    // The center point of the NEW view state (where the map is trying to move)
+    const newCenter = [newViewState.longitude, newViewState.latitude];
+
     // Only update the view state if the center is inside the geofence
     if (turf.booleanPointInPolygon(newCenter, GEOFENCE)) {
-      setViewState(viewState);
+      setViewState(newViewState); // CHQ: Gemini AI set the new view state from the event
     }
-  }, []);
-
+  }, []); // CHQ: Gemini AI made Dependency array to be empty
   return (
     <Map
       mapboxAccessToken={MAPBOX_TOKEN}
