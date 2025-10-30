fd1900c2c47acf0a317200360fa8abd67964a6bd
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-10-30 03:09:43 -0400

    chore: cleanup comments in codegen.ts
    Cleanup unnecessary comments in codegen.ts

--- codegen.ts
+++ codegen.ts
@@ -1,24 +1,17 @@
-// import type { CodegenConfig } from "@graphql-codegen/cli";
-// const config: CodegenConfig = {
-//   overwrite: true,
-//   schema: "http://localhost:4000/graphql", // Replace with your GraphQL server endpoint documents: "src/**/*.graphql", // Path to your client-side query files generates: { "src/generated/graphql.tsx": { // Path where the generated types and hooks will be placed plugins: [ 'typescript', 'typescript-operations', 'typescript-react-apollo', ], config: { withHooks: true, }, }, },
-// };
-// export default config;
-
 // // Note: If you write queries inside of TypeScript/JavaScript files using gql tags, update the documents field to use a glob that matches those files, such as "src/**/*.{ts,tsx}".
 
 import type { CodegenConfig } from "@graphql-codegen/cli";
 
 const config: CodegenConfig = {
-  // 1. Define where your GraphQL schema is located
-  // This could be a file path (e.g., 'schema.graphql') or a URL to a running server.
   // schema: "http://localhost:4000/graphql",
   // schema: "./app/pages/schema/queries/locations.graphql",
+  // 1. Define where your GraphQL schema is located.
   schema: "./app/schema.graphql",
 
-  // 2. Define where your GraphQL documents (queries, mutations, etc.) are
   // documents: "app/**/*.graphql",
   documents: "app/pages/**/*.graphql",
+  // 2. Define where your GraphQL documents (queries, mutations, etc.) are.
+  documents: "app/**/*.graphql",
 
   // 3. The essential 'generates' field
   generates: {
@@ -31,7 +24,6 @@ const config: CodegenConfig = {
         "typescript-react-apollo",
       ],
       // Optional: configuration options for the plugins
-
       config: {
         // e.g., 'withHooks' or 'skipTypename'
       },
