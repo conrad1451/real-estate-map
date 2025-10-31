5d36dddbec432e95c3b57b5c9243e59db4e58947
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-10-31 18:35:31 -0400

    feat: include apollo-client

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -5,12 +5,22 @@ import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 //   cache: new InMemoryCache(),
 // });
 
+// const client = new ApolloClient({
+//   // link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
+//   link: new HttpLink({
+//     uri: "https://graphql.eng.meridiancapital.com/graphql",
+//   }),
+
+//   cache: new InMemoryCache(),
+// });
+
 const client = new ApolloClient({
   // link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
   link: new HttpLink({
-    uri: "https://graphql.eng.meridiancapital.com/graphql",
+    uri: "/api/graphql", // Or your external GraphQL API URL
   }),
 
   cache: new InMemoryCache(),
 });
+
 export default client;
