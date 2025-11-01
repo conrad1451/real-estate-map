cd7ea05274167b5d201dbe785b82dcecb1cfcaa1
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-11-01 18:07:12 -0400

    chore: remove Apollo server code
    Remove apollo server code since this nextjs project must communicate with an external GraphQL server.

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -1,23 +1,9 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
-// import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
-// const client = new ApolloClient({
-//   uri: "/api/graphql", // Or your external GraphQL API URL
-//   cache: new InMemoryCache(),
-// });
-
-// const client = new ApolloClient({
-//   // link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
-//   link: new HttpLink({
-//     uri: "https://graphql.eng.meridiancapital.com/graphql",
-//   }),
-
-//   cache: new InMemoryCache(),
-// });
 
 const client = new ApolloClient({
   // link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
   link: new HttpLink({
-    uri: "/api/graphql", // Or your external GraphQL API URL
+    uri: "https://graphql.eng.meridiancapital.com/graphql",
   }),
 
   cache: new InMemoryCache(),
