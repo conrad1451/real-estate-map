e2681c76b593fa3c29f360496c125b4164a8b635
Conrad Hansen-Quartey <Chansenq@gmail.com>
2025-11-01 18:03:00 -0400

    feat: store combo site and server
    Storing combo site and server where GraphQL server and NextJS frontend are hosted in a single NextJS project.
    
    This was before realizing the GraphQL server we were to use was an external server we had to access by URL, but for which the URL was not available.

--- app/lib/apollo-client.ts
+++ app/lib/apollo-client.ts
@@ -1,18 +1,5 @@
 import { ApolloClient, HttpLink, InMemoryCache } from "@apollo/client";
 // import { ApolloClient, HttpLink, InMemoryCache, gql } from "@apollo/client";
-// const client = new ApolloClient({
-//   uri: "/api/graphql", // Or your external GraphQL API URL
-//   cache: new InMemoryCache(),
-// });
-
-// const client = new ApolloClient({
-//   // link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
-//   link: new HttpLink({
-//     uri: "https://graphql.eng.meridiancapital.com/graphql",
-//   }),
-
-//   cache: new InMemoryCache(),
-// });
 
 const client = new ApolloClient({
   // link: new HttpLink({ uri: "https://flyby-router-demo.herokuapp.com/" }),
